57258b72b889de84c5ced7325312618c863d79ae
Antonov Igor <Gosha.8686@gmail.com>
2020-10-20 02:58:51 +0300

    avatar

--- src/components/Profile/Profileinfo/ProfileInfo.jsx
+++ src/components/Profile/Profileinfo/ProfileInfo.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import HookProfileStatus from "./HookProfileStatus";
+import userPhoto from "../../../assets/img/user.webp";
 
-const ProfileInfo = ({profile,status,updateStatus}) => {
+
+const ProfileInfo = ({isOwner,profile,status,updateStatus,savePhoto}) => {
     if (!profile){
         return <Preloader/>
     }
+
+    const onMainPhotoSelected = (e)=>{
+        if (e.target.files.length){
+            savePhoto(e.target.files[0])
+        }
+    }
+
     return (
         <div>
             <div className={s.descriptionBlock}>
-                <img src={profile.photos.large}/>
+                <img src={profile.photos.large ||  userPhoto} className={s.mainPhoto}/>
+                {isOwner && <input type={'file'} onChange={onMainPhotoSelected}/>}
             <HookProfileStatus status={status} updateStatus={updateStatus}/>
             </div>
         </div>

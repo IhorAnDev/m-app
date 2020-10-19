5aa5e4dc4faea43dc34fc05c3488782255f55fa2
Antonov Igor <Gosha.8686@gmail.com>
2020-10-19 13:45:51 +0300

    Updates

---
+++ precache-manifest.f5e4471f16c0f715c574dc5b67c42f38.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103fe4eb1fb6f998e36c0cdf4a6f1800",
+    "url": "/m-app/index.html"
+  },
+  {
+    "revision": "6e28abc96c80794ae5cc",
+    "url": "/m-app/static/css/3.e0903ac2.chunk.css"
+  },
+  {
+    "revision": "36918136501eb7463414",
+    "url": "/m-app/static/css/4.b7e3707a.chunk.css"
+  },
+  {
+    "revision": "d8698412b907e3b654c1",
+    "url": "/m-app/static/css/main.6bd682c1.chunk.css"
+  },
+  {
+    "revision": "7e1ba6f7c8ed8bf6356b",
+    "url": "/m-app/static/js/2.09a9d483.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/m-app/static/js/2.09a9d483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e28abc96c80794ae5cc",
+    "url": "/m-app/static/js/3.9477592a.chunk.js"
+  },
+  {
+    "revision": "36918136501eb7463414",
+    "url": "/m-app/static/js/4.11fd6478.chunk.js"
+  },
+  {
+    "revision": "d8698412b907e3b654c1",
+    "url": "/m-app/static/js/main.2324a623.chunk.js"
+  },
+  {
+    "revision": "3dcbb0859c43f81aabc3",
+    "url": "/m-app/static/js/runtime-main.af393e2b.js"
+  },
+  {
+    "revision": "da7482baaf478a68c7c6295890770f1c",
+    "url": "/m-app/static/media/giphy.da7482ba.webp"
+  },
+  {
+    "revision": "00ca3d080e452925bc5561db21eb58b0",
+    "url": "/m-app/static/media/user.00ca3d08.webp"
+  }
+]);
